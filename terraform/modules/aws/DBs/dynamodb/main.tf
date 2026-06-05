123c8ff66fbb510ef9a4ab07c55d5766ac2b913c
Kareem El-Giushy <kareem.64123@gmail.com>
2026-06-05 19:57:25 +0300

    Change Some Database Configuration

--- terraform/modules/aws/DBs/dynamodb/main.tf
+++ terraform/modules/aws/DBs/dynamodb/main.tf
@@ -1,10 +1,11 @@
 resource "aws_dynamodb_table" "cart" {
   name         = "${var.project_name}-cart"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "userId"
+
+  hash_key = "itemID"
 
   attribute {
-    name = "userId"
+    name = "itemID"
     type = "S"
   }
 
@@ -13,29 +14,9 @@ resource "aws_dynamodb_table" "cart" {
     enabled        = true
   }
 
-  point_in_time_recovery {
-    enabled = true
-  }
-
   server_side_encryption {
     enabled = true
   }
-
-  tags = {
-    Project   = var.project_name
-    Service   = "cart"
-    ManagedBy = "Terraform"
-  }
-}
-
-
-resource "aws_vpc_endpoint" "dynamodb" {
-  vpc_id            = var.vpc_id
-  service_name      = "com.amazonaws.${var.aws_region}.dynamodb"
-  vpc_endpoint_type = "Gateway"
-
-  # Reference a single private route table
-  route_table_id = var.private_route_table_id
 
   tags = {
     Project   = var.project_name
